94eec66378ecf2af46db2f4cb217bcc08c704bdb
yogeshdnumb <dyogesh.official@gmail.com>
2024-09-24 20:45:30 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: ["https://codebugger.netlify.app"] }));
 
 app.use("/", indexRouter);
 
