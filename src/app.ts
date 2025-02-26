75d1d21df08257ff115b71dae39aab83b798c9ce
yogeshdnumb <dyogesh.official@gmail.com>
2025-02-26 11:09:40 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,15 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["https://codebugger.netlify.app"] }));
+// app.use(cors({ origin: ["https://codebugger.netlify.app"] }));
+app.use((req, res, next) => {
+  // res.setHeader("bypass-tunnel-reminder", "Express");
+  res.header("Access-Control-Allow-Origin", "*"); // Change * to specific origin if needed
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // res.setHeader("Content-Type", "application/json");
+  next(); // Pass control to the next handler
+});
 
 app.use("/", indexRouter);
 
